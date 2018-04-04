5cde319f5a0e8da5ed18d079ab4a443b507d7855
Hilton <hdcs05@gmail.com>
2018-04-04 16:47:35 -0400

    deleted clear button, added radio group and buttons for question 1, added seekbar for question 2

--- app/src/main/java/com/example/android/androidtriviaquiz/MainActivity.java
+++ app/src/main/java/com/example/android/androidtriviaquiz/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.android.androidtriviaquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    /* This 2 global variables are for the seekbar on question #2 */
+    TextView gNumberSeekbar;
+    SeekBar gSeekbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gNumberSeekbar = (TextView)findViewById(R.id.number_question2);
+        gSeekbar = (SeekBar)findViewById(R.id.seekbar_question2);
+        gSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                String seekbarValue = String.valueOf(i);
+                gNumberSeekbar.setText(seekbarValue);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
     }
 }
